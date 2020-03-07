c1be1bf29675c0f4aad113e6a5a6d2a51a9a98d3
alexandra tsiounis <AlexT@alexandras-MacBook-Air-4.local>
2020-03-06 22:22:22 -0500

    ok

---
+++ myconversions.py
@@ -0,0 +1,16 @@
+print("My module is running")
+
+def fromcelcius():
+    usertemp = (input("Enter the temp you want to convert to degrees fahrenheit: "))
+    temp = float(usertemp)
+    print(type(temp))
+    return((temp(*1.8)+32))
+
+def fromfahrenheit(value):
+        return(value-32)/1.8
+        
+    
+    
+        
+
+        
